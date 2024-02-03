44848004a67e42a29023ea51479b39ea8485611d
Marco-Martins-97 <154925538+Marco-Martins-97@users.noreply.github.com>
2024-02-03 21:26:10 +0000

    Add files via upload

--- game.py
+++ game.py
@@ -1,35 +1,51 @@
-# #Obelisk v.1.10
-import time
+# #Obelisk v.1.10.1
+from datetime import datetime, timedelta
 import village as v
 import configurations as config
 
 class Game:
     def __init__(self, data):
+        logout_time, data = data
         wood, clay, iron, progress1, progress2, progress_time, headquartes, timbercamp, claypit, ironmine, farm, warehouse = data           #unpack the data
-        self.wood = float(wood)
-        self.clay = float(clay)
-        self.iron = float(iron)
+        
+        self.village_level = [int(headquartes), int(timbercamp), int(claypit), int(ironmine), int(farm), int(warehouse)]
+        self.game_speed = config.game_speed             #Get the game speed
+        
+        self.wood_p = v.calculate_factor(1, self.village_level[1]) * self.game_speed
+        self.clay_p = v.calculate_factor(2, self.village_level[2]) * self.game_speed
+        self.iron_p = v.calculate_factor(3, self.village_level[3]) * self.game_speed
+        self.farm = v.calculate_factor(4, self.village_level[4])
+        self.warehouse = v.calculate_factor(5, self.village_level[5])
 
+        
         self.progress1 = int(progress1)
         self.progress2 = int(progress2)
         self.progress_time = int(progress_time)
 
-        self.village_level = [int(headquartes), int(timbercamp), int(claypit), int(ironmine), int(farm), int(warehouse)]
 
-        self.wood_p = 0
-        self.clay_p = 0
-        self.iron_p = 0
-        self.farm = 0
-        self.warehouse = 0
+        #calculate offline time
 
-        self.start_delay = time.time()                  #start the timers
-        
-        self.start_progress = time.time()
+        offline_time = datetime.now() - logout_time             #time from the last logout to login
+        offline_seconds = offline_time.total_seconds()          #offline time in seconds
+        print(f'OFLINE: {offline_time}')
+        print(f'OFLINE_S: {offline_seconds}')
 
-        self.game_speed = config.game_speed             #Get the game speed
+        #add offline resources
+        self.wood = min(wood + (self.wood_p/3600) * offline_seconds, self.warehouse)
+        self.clay = min(clay + (self.wood_p/3600) * offline_seconds, self.warehouse)
+        self.iron = min(iron + (self.wood_p/3600) * offline_seconds, self.warehouse)
 
-        self.start_progress = (self.progress_time - v.calculate_time(self.progress1, self.village_level[self.progress1], self.village_level[0])/self.game_speed) + time.time()          #load the remain time from database and apply it to progress
-        self.update()
+
+
+
+        ###################################################################################################################################################
+
+
+        self.start_delay = datetime.now()                  #start the timers  
+        self.start_progress = datetime.now()
+
+
+        #self.start_progress = (self.progress_time - v.calculate_time(self.progress1, self.village_level[self.progress1], self.village_level[0])/self.game_speed) + datetime.now()          #load the remain time from database and apply it to progress
         
     #Update the calculated values
     def update(self):
@@ -46,18 +62,18 @@ class Game:
         self.iron = min(self.iron + self.iron_p/3600, self.warehouse)
         
     #Game ticks
-    def delay(self, _time):
-        if time.time() > _time + self.start_delay:
-            self.start_delay = time.time()
+    def delay(self, time):
+        if datetime.now() - self.start_delay > timedelta(seconds=time):
+            self.start_delay = datetime.now()
             return True
 
         
     #Progress1 timer
     def progress_timer(self, _time):
-        if time.time() > _time + self.start_progress:
-            self.start_progress = time.time()
+        if datetime.now() > _time + self.start_progress:
+            self.start_progress = datetime.now()
             return True
-        self.progress_time = (self.start_progress + _time) - time.time()        #save the time left
+        self.progress_time = (self.start_progress + _time) - datetime.now()        #save the time left
     
     #Pack the data
     def get_data(self):
@@ -101,7 +117,7 @@ class Game:
         if self.progress1 == -1:
             self.sub_resources(building_idx, 1)     #subtract the resources
             self.progress1 = building_idx           #put the building in the progress
-            self.start_progress = time.time()       #update the timer
+            self.start_progress = datetime.now()       #update the timer
         elif self.progress2 == -1:
             self.sub_resources(building_idx, 2)     #subtract the resources
             self.progress2 = building_idx           #put the building in the progress
